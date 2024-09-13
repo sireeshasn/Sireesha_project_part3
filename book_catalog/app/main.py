075e1ca3de902bf2bb30788a1bea50f3e971cc62
Sireesha <sireesha2622@gmail.com>
2024-09-13 20:23:46 +1000

    commit

--- book_catalog/app/main.py
+++ book_catalog/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Depends, status
 from sqlalchemy.orm import Session
-from .database import SessionLocal, engine
-from . import crud, models, schemas
+from app.database import SessionLocal, engine
+from app import crud, models, schemas
 
 models.Base.metadata.create_all(bind=engine)
 
